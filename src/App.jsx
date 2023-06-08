ef0f8f6a1fa33b7b05d6e819e3e029d9892719b8
jorgeduje <jorgeduje4@gmail.com>
2023-06-08 20:49:16 -0300

    subiendo los primeros archivos

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Footer from "./Footer";
+import Navbar from "./Navbar";
+import ProductList from "./ProductsList";
+
+function App() {
+
+
+  return (
+    <div>
+      <Navbar />
+      <ProductList />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
