1e396012f9d8b3a160e94543a7a7362f1bdbd89e
jorgeduje <jorgeduje4@gmail.com>
2023-07-11 21:00:58 -0300

    se trabaja con eventos, y se refactorizan rutas

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,11 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import ItemListContainer from "./components/pages/itemList/ItemListContainer";
-import ItemDetailContainer from "./components/pages/itemDetail/ItemDetailContainer";
-import CartContainer from "./components/pages/cart/CartContainer";
-import Navbar from "./components/layout/navbar/Navbar";
+import { BrowserRouter } from "react-router-dom";
+
+import AppRouter from "./router/AppRouter";
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-
-        <Route element={<Navbar />}>
-          <Route path="/" element={<ItemListContainer />} />
-          <Route path="/category/:categoryName" element={<ItemListContainer />} />
-          <Route path="/itemDetail/:id" element={<ItemDetailContainer />} />
-          <Route path="/cart" element={<CartContainer />} />
-        </Route>
-
-        <Route path="*" element={<h1>404 - Not found</h1>} />
-      </Routes>
+     <AppRouter />
     </BrowserRouter>
   );
 }
