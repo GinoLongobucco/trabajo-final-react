1eabf31e463882372bb174e66807fa4d23d4d3c7
jorgeduje <jorgeduje4@gmail.com>
2023-07-25 20:29:36 -0300

    se implementa create y update de firebase

--- src/components/pages/checkout/CheckoutContainer.jsx
+++ src/components/pages/checkout/CheckoutContainer.jsx
@@ -1,48 +1,85 @@
-import { useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { useContext, useState } from "react";
+import { CartContext } from "../../../context/CartContext";
+import { db } from "../../../firebaseConfig";
+import {
+  addDoc,
+  collection,
+  serverTimestamp,
+  updateDoc,
+  doc,
+} from "firebase/firestore";
+import { Button } from "@mui/material";
 
 const CheckoutContainer = () => {
+  const { cart, getTotalPrice } = useContext(CartContext);
 
-  const navigate = useNavigate()
+  const [orderId, setOrderId] = useState("");
 
   const [userData, setUserData] = useState({
     name: "",
-    lastName: "",
+    phone: "",
+    email: "",
   });
+  let total = getTotalPrice();
 
-  const funcionDelFormulario = (evento) => {
+  const handleSubmit = (evento) => {
     evento.preventDefault();
 
     // AXIOS.POST("dasdasdas", userData)
-    console.log(userData);
+    let order = {
+      buyer: userData,
+      items: cart,
+      total,
+      date: serverTimestamp(),
+    };
 
-    // ACA QUIERO VOLVER AL HOME
-    navigate("/")
+    // CREAR UNA ORDEN DE COMPRA
+    let ordersCollections = collection(db, "orders");
+    addDoc(ordersCollections, order).then((res) => setOrderId(res.id));
+
+    // MODIFICAR TODOS LOS PRODUCTOS EN SU STOCK
+    cart.forEach((elemento) => {
+      updateDoc(doc(db, "products", elemento.id), {
+        stock: elemento.stock - elemento.quantity,
+      });
+    });
   };
 
-  const funcionDeLosInput = (evento) => {
+  const handleChange = (evento) => {
     setUserData({ ...userData, [evento.target.name]: evento.target.value });
   };
 
   return (
     <div>
       <h1>Checkout</h1>
-      <form onSubmit={funcionDelFormulario}>
-        <input
-          type="text"
-          placeholder="ingrese su nombre"
-          name="name"
-          onChange={funcionDeLosInput}
-        />
-        <input
-          type="text"
-          placeholder="ingrese su apellido"
-          name="lastName"
-          onChange={funcionDeLosInput}
-        />
-        <button type="submit">Enviar</button>
-        <button type="button">Cancelar</button>
-      </form>
+
+      {!orderId ? (
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            placeholder="ingrese su nombre"
+            name="name"
+            onChange={handleChange}
+          />
+          <input
+            type="text"
+            placeholder="ingrese su telefono"
+            name="phone"
+            onChange={handleChange}
+          />
+          <input
+            type="text"
+            placeholder="ingrese su email"
+            name="email"
+            onChange={handleChange}
+          />
+          <Button variant="contained" type="submit">
+            Comprar
+          </Button>
+        </form>
+      ) : (
+        <h3>Su numero de compra: {orderId} </h3>
+      )}
     </div>
   );
 };
--- src/productsMock.js
+++ src/productsMock.js
@@ -1,6 +1,6 @@
 export const products = [
   {
-    id: 1,
+   
     title: "Nike",
     price: 15000,
     stock: 2,
@@ -9,7 +9,6 @@ export const products = [
     img: "https://res.cloudinary.com/dnqfh2chg/image/upload/v1669326932/free-metcon-4-zapatillas-de-entrenamiento-TQMCZg_fvia3m.png",
   },
   {
-    id: 2,
     title: "wilson",
     price: 14500,
     stock: 5,
@@ -18,7 +17,6 @@ export const products = [
     img: "https://res.cloudinary.com/dnqfh2chg/image/upload/v1669430010/foc364fsakkzmausrovn.jpg",
   },
   {
-    id: 3,
     title: "Puma",
     price: 19000,
     stock: 1,
@@ -28,7 +26,6 @@ export const products = [
     img: "https://res.cloudinary.com/dnqfh2chg/image/upload/v1669430165/images_lujpmr.jpg",
   },
   {
-    id: 4,
     title: "Jaguar",
     price: 17800,
     stock: 5,
